d5676b0cba1a13e6faa60df0bdf2b2816c2495b8
Kaleem Peeroo <Kaleem.Peeroo@city.ac.uk>
2025-04-15 14:46:47 +0100

    Added UT for validate config

--- tests/test_main.py
+++ tests/test_main.py
@@ -6,6 +6,71 @@ class TestMain:
         from main import validate_config
         from tests.configs.normal import LD_DATASETS
 
-        assert validate_config(LD_DATASETS) == True
+        assert validate_config(LD_DATASETS) is True
 
+    def test_validate_config_with_empty_dict(self):
+        from main import validate_config
+
+        with pytest.raises(ValueError):
+            validate_config({})
+
+    def test_validate_with_empty_list(self):
+        from main import validate_config
+
+        with pytest.raises(ValueError):
+            validate_config([])
+
+    def test_validate_config_with_invalid_type(self):
+        from main import validate_config
+
+        with pytest.raises(ValueError):
+            validate_config("invalid_type")
         
+    def test_validate_config_with_missing_key(self):
+        from main import validate_config
+
+        with pytest.raises(ValueError):
+            validate_config( [ {"name": "test"} ] )
+
+    def test_validate_config_with_invalid_key(self):
+        from main import validate_config
+
+        with pytest.raises(ValueError):
+            validate_config( [ {
+                "name": "test", 
+                "exp_folders": "value",
+                "ap_config": "",
+                "invalid_key": ""
+            } ] )
+
+    def test_validate_config_with_invalid_value(self):
+        from main import validate_config
+
+        with pytest.raises(ValueError):
+            validate_config( [ {
+                "name": "test", 
+                "exp_folders": 123,
+                "ap_config": "",
+                "dataset_path": ""
+            } ] )
+
+    def test_validate_config_with_invalid_dataset_path(self):
+        from main import validate_config
+
+        with pytest.raises(ValueError):
+            validate_config( [ {
+                "name": "test", 
+                "exp_folders": "",
+                "ap_config": "",
+                "dataset_path": 123
+            } ] )
+
+    def test_validate_config_with_invalid_ap_config(self):
+        from main import validate_config
+
+        assert validate_config( [ {
+            "name": "test", 
+            "exp_folders": "./data/test_campaign/",
+            "ap_config": "",
+            "dataset_path": "./output/test_campaign.parquet"
+        } ] ) is True
