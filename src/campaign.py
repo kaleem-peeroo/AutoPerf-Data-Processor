1da7555efca8041cb73592d6ca0116be57f7aa3c
Kaleem Peeroo <Kaleem.Peeroo@city.ac.uk>
2025-04-15 16:42:56 +0100

    Created a new create dataset function in campaign

--- src/campaign.py
+++ src/campaign.py
@@ -33,20 +33,20 @@ class Campaign:
         self.data_exp_list  = []    # experiment list from the data dir experiment names
         self.missing_exps   = []    # Missing experiment list
 
-    def __rich_repr__(self):
-        yield "data_dir", self.raw_datadir
-        yield "apconf_path", self.apconf_path
-        yield "config", self.config
-        yield "qos_settings", self.qos_variations
-        yield "qos_exp_list", self.qos_exp_list
-        yield "data_exp_list", self.data_exp_list
-        yield "missing_exps", self.missing_exps
+    def get_raw_datadir(self):
+        if not self.raw_datadir:
+            raise Exception("No raw data directory provided")
 
-    def get_data_dir(self):
+        if not isinstance(self.raw_datadir, str):
+            raise ValueError(f"Raw data directory must be a string: {self.raw_datadir}")
+
+        if self.raw_datadir == "":
+            raise ValueError("Raw data directory must not be empty")
+
+        if "~" in self.raw_datadir:
+            self.raw_datadir = os.path.expanduser(self.raw_datadir)
+
         return self.raw_datadir
-
-    def get_apconf_path(self):
-        return self.apconf_path
 
     def get_dataset_path(self):
         if not self.ds_output_path:
@@ -63,98 +63,37 @@ class Campaign:
 
         return self.ds_output_path
 
-    def set_data_dir(self, data_dir):
-        self.raw_datadir = data_dir
-
-    def set_apconf_path(self, apconf_path):
-        self.apconf_path = apconf_path
-
-    def validate_args(self):
-        lg.info("Validating campaign args...")
-
-        if not self.raw_datadir:
-            lg.critical("No data dir provided")
-            return False
-
-        if "~" in self.raw_datadir:
-            self.raw_datadir = os.path.expanduser(self.raw_datadir)
-
-        if "~" in self.apconf_path:
-            self.apconf_path = os.path.expanduser(self.apconf_path)
-
-        if not os.path.exists(self.data_dir):
-            logger.critical(f"Data dir does not exist: {self.data_dir}")
-            return False
-
-        if self.apconf_path != "":
-            if not os.path.exists(self.apconf_path):
-                logger.critical(f"AP config path does not exist: {self.apconf_path}")
-                return False
-
-            if not os.path.isfile(self.apconf_path):
-                logger.critical(f"AP config path is not a file: {self.apconf_path}")
-                return False
-
-            if not self.apconf_path.endswith(".toml"):
-                logger.critical(f"AP config path does not end with .toml: {self.apconf_path}")
-                return False
-
-        return True
-
-    def get_files_from_pathlib(
-        self, 
-        path: Path = Path('.'),
-    ):
-        ls_fpaths = []
-        for entry in path.iterdir():
-            if entry.is_dir():
-                ls_fpaths.extend(self.get_files_from_pathlib(entry))
-            else:
-                ls_fpaths.append(str(entry))
-
-        return ls_fpaths
-
     def create_dataset(self):
-        dataset_path = self.get_dataset_path()
-        if not dataset_path.endswith(".parquet"):
-            logger.warning(f"{dataset_path} does NOT end with .parquet. Appending .parquet to the end of the path")
-            dataset_path = f"{dataset_path}.parquet"
+        """
+        1. Go through each item in the experiment directory
+        2. Read the csv file
+        3. Add the experiment name
+        4. Add the qos settings to the dataframe
+        5. Add to a big ds df
+        """
+        s_raw_datadir = self.get_raw_datadir()
 
-        if not self.data_dir:
-            raise Exception("No data dir")
+        ld_exp_names_and_paths = self.get_experiments(s_raw_datadir)
 
-        if not os.path.exists(self.data_dir):
-            raise Exception(f"Data dir does not exist: {self.data_dir}")
+    def old_create_dataset(self):
+        lg.debug("Creating dataset...")
 
-        if len(os.listdir(self.data_dir)) == 0:
-            raise Exception(f"Data dir is empty: {self.data_dir}")
-
-        logger.debug("Creating dataset...")
-
-        exp_dirs = os.listdir(self.data_dir)
+        exp_dirs = os.listdir(self.raw_datadir)
         if '.DS_Store' in exp_dirs:
             exp_dirs.remove('.DS_Store')
 
         exp_dirs = [
             exp_dir for exp_dir in exp_dirs \
-                    if os.path.isdir(os.path.join(self.data_dir, exp_dir)) or
+                    if os.path.isdir(os.path.join(self.raw_datadir, exp_dir)) or
                     exp_dir.endswith(".csv")
         ]
 
-        incomplete_exp_names = []
-
         dataset_df = pd.DataFrame()
-
-        if os.path.exists(dataset_path):
-            now_timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-            dataset_path = f"{dataset_path.replace('.parquet', '')}_{now_timestamp}.parquet"
-            logger.warning(f"Dataset already exists at {dataset_path}. Writing to {dataset_path}")
-
-        exp_dirs = [os.path.join(self.data_dir, exp_dir) for exp_dir in exp_dirs]
-        logger.debug(f"Found {len(exp_dirs)} experiment directories in {self.data_dir}")
+        exp_dirs = [os.path.join(self.raw_datadir, exp_dir) for exp_dir in exp_dirs]
+        lg.debug(f"Found {len(exp_dirs)} experiment directories in {self.raw_datadir}")
 
         for index, exp_dir in enumerate(exp_dirs):
-            logger.info(
+            lg.info(
                 "[{}/{}] Processing {}".format(
                     index + 1,
                     len(exp_dirs),
@@ -174,21 +113,14 @@ class Campaign:
                 ls_fpaths = self.get_files_from_pathlib(Path(exp_dir))
                 ls_csv_fpaths = [fpath for fpath in ls_fpaths if fpath.endswith(".csv")]
 
-                # csv_files = [os.path.join(
-                #     exp_dir, 
-                #     file
-                # ) for file in os.listdir(exp_dir) if file.endswith(".csv")]
-
                 if len(ls_csv_fpaths) == 0:
-                    logger.warning(f"No csv files found in {exp_dir}")
-                    incomplete_exp_names.append(os.path.basename(exp_dir))
+                    lg.warning(f"No csv files found in {exp_dir}")
                     continue
 
                 csv_filenames = [os.path.basename(file) for file in ls_csv_fpaths]
 
                 if "pub_0.csv" not in csv_filenames:
-                    logger.warning(f"pub_0.csv not found in {exp_dir}")
-                    incomplete_exp_names.append(os.path.basename(exp_dir))
+                    lg.warning(f"pub_0.csv not found in {exp_dir}")
                     continue
 
                 exp = Experiment(exp_name)
@@ -196,18 +128,15 @@ class Campaign:
                 exp.set_sub_files([file for file in ls_csv_fpaths if "sub_" in file])
 
                 if not exp.validate_files():
-                    incomplete_exp_names.append(exp_name)
                     continue
 
                 exp.parse_pub_file()
                 exp.parse_sub_files()
 
                 if exp.get_subs_df() is None:
-                    incomplete_exp_names.append(exp_name)
                     continue
 
                 if exp.get_pub_df() is None:
-                    incomplete_exp_names.append(exp_name)
                     continue
 
                 qos = exp.get_qos()
@@ -223,35 +152,31 @@ class Campaign:
                 exp_df['experiment_name'] = exp_name
                 exp_df = aggregate_across_cols(exp_df, ['avg', 'total'])
 
-
             dataset_df = pd.concat([dataset_df, exp_df], ignore_index=True)
 
             if index % 10 == 0 and index != 0:
                 if len(dataset_df) > 0:
                     dataset_df.to_parquet(dataset_path)
-                    logger.info(f"Written to {dataset_path}")
+                    lg.info(f"Written to {dataset_path}")
 
         if len(dataset_df) == 0:
-            logger.error(f"No data found in the dataset")
-            return incomplete_exp_names
+            lg.error(f"No data found in the dataset")
 
         dataset_df.to_parquet(dataset_path)
-        logger.info(f"Dataset written to {dataset_path}")
+        lg.info(f"Dataset written to {dataset_path}")
 
         # Print how many experiments are in the dataset
         exp_count = dataset_df['experiment_name'].nunique()
-        logger.info(f"Dataset has {exp_count} experiments")
-            
-        return incomplete_exp_names
+        lg.info(f"Dataset has {exp_count} experiments")
 
     def generate_qos_exp_list(self):
-        if not self.qos_settings:
+        if not self.qos_variations:
             raise Exception("No qos_settings in config")
 
-        if not isinstance(self.qos_settings, dict):
-            raise ValueError(f"QoS config must be a dict: {self.qos_settings}")
+        if not isinstance(self.qos_variations, dict):
+            raise ValueError(f"QoS config must be a dict: {self.qos_variations}")
 
-        if self.qos_settings == {}:
+        if self.qos_variations == {}:
             raise ValueError("QoS config must not be empty")
 
         required_keys = [
@@ -265,15 +190,15 @@ class Campaign:
             'latency_count'
         ]
 
-        keys = self.qos_settings.keys()
+        keys = self.qos_variations.keys()
         if len(keys) == 0:
             raise ValueError("No options found for qos")
 
         for key in required_keys:
-            if key not in self.qos_settings:
+            if key not in self.qos_variations:
                 raise ValueError(f"QoS config must have {key}")
 
-        values = self.qos_settings.values()
+        values = self.qos_variations.values()
         if len(values) == 0:
             raise ValueError("No values found for QoS")
 
@@ -285,34 +210,34 @@ class Campaign:
         combination_dicts = [dict(zip(keys, combination)) for combination in combinations]
 
         if len(combination_dicts) == 0:
-            raise ValueError(f"No combinations were generated from the QoS values:\n\t {self.qos_settings}")
+            raise ValueError(f"No combinations were generated from the QoS values:\n\t {self.qos_variations}")
 
         self.qos_exp_list = [get_qos_name(combination) for combination in combination_dicts]
 
     def get_missing_exps(self):
         self.generate_qos_exp_list()
 
-        self.data_exp_list = [os.path.basename(path) for path in os.listdir(self.data_dir) if '.DS_Store' not in path]
+        self.data_exp_list = [os.path.basename(path) for path in os.listdir(self.raw_datadir) if '.DS_Store' not in path]
 
         missing_tests = list(set(self.qos_exp_list) - set(self.data_exp_list))
 
         if len(missing_tests) == len(self.qos_exp_list):
-            logger.warning("All tests are missing. The names are probably wrong.")
+            lg.warning("All tests are missing. The names are probably wrong.")
             self.data_exp_list = [item.replace("PUB", "P") for item in self.data_exp_list]
             self.data_exp_list = [item.replace("SUB", "S") for item in self.data_exp_list]
             missing_tests = list(set(self.qos_exp_list) - set(self.data_exp_list))
 
-        logger.info("Expected {} experiments according to {}.".format(
+        lg.info("Expected {} experiments according to {}.".format(
             len(self.qos_exp_list), 
             self.apconf_path
         ))
 
-        logger.info("Found {} experiments in {}.".format(
+        lg.info("Found {} experiments in {}.".format(
             len(self.data_exp_list),
-            self.data_dir
+            self.raw_datadir
         ))
 
-        logger.info(f"Missing {len(missing_tests)} experiments.")
+        lg.info(f"Missing {len(missing_tests)} experiments.")
 
         return missing_tests
 
@@ -329,7 +254,7 @@ class Campaign:
         if 'qos_settings' not in self.config.keys():
             raise Exception("No qos_settings in config")
 
-        self.qos_settings = self.config['qos_settings']
+        self.qos_variations = self.config['qos_settings']
         self.missing_exps = self.get_missing_exps()
 
         # Add missing_exps to incomplete_exp_names and remove duplicates
@@ -338,7 +263,7 @@ class Campaign:
         fixed_exp_names = [exp.replace("SUB_", "S_") for exp in fixed_exp_names]
 
         if len(self.missing_exps) > 0:
-            logger.info(f"Generating missing test config with {len(self.missing_exps)} missing tests.")
+            lg.info(f"Generating missing test config with {len(self.missing_exps)} missing tests.")
 
         new_ap_config = self.config.copy()
         new_ap_config['experiment_names'] = fixed_exp_names
@@ -348,10 +273,10 @@ class Campaign:
         with open(new_ap_config_path, "w") as f:
             toml.dump(new_ap_config, f)
 
-        logger.info(f"New apconf file generated at {new_ap_config_path}")
+        lg.info(f"New apconf file generated at {new_ap_config_path}")
 
     def generate_config(self):
-        logger.info("Generating config...")
+        lg.info("Generating config...")
 
         if not self.apconf_path:
             raise Exception("No apconf path provided")
@@ -368,7 +293,7 @@ class Campaign:
                 raise Exception("No campaigns defined in apconf")
 
             elif len(campaign_configs) > 1:
-                logger.warning("Found multiple campaigns in {}. Using the first one.")
+                lg.warning("Found multiple campaigns in {}. Using the first one.")
 
             self.config = ap_config['campaigns'][0]
 
