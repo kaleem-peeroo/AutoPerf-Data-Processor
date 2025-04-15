dec4d241e77ba44752784452740fdc99f0f1230f
Kaleem Peeroo <Kaleem.Peeroo@city.ac.uk>
2025-04-15 16:20:01 +0100

    Updated UT for campaign init with normal

--- tests/test_campaign.py
+++ tests/test_campaign.py
@@ -8,9 +8,16 @@ class TestCampaign:
         ld_ds_config = LD_DATASETS
 
         for d_ds in ld_ds_config:
-            campaign = Campaign(d_ds)
-            assert campaign is not None
-            assert isinstance(campaign, Campaign)
-            assert campaign.ds_output_path == d_ds['dataset_path']
-            assert campaign.apconf_path == d_ds['ap_config']
-            assert campaign.raw_datadir == d_ds['exp_folders']
+            o_c = Campaign(d_ds)
+            assert o_c is not None
+            assert isinstance(o_c, Campaign)
+
+            assert o_c.ds_output_path == d_ds['dataset_path']
+            assert o_c.apconf_path == d_ds['ap_config']
+            assert o_c.raw_datadir == d_ds['exp_folders']
+            assert o_c.ds_df is None
+            assert o_c.config is None
+            assert o_c.qos_variations == {}
+            assert o_c.qos_exp_list == []
+            assert o_c.data_exp_list == []
+            assert o_c.missing_exps == []
