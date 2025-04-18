f1dbc0da77badbc9c5698638c2ee2a1110edc4df
Kaleem Peeroo <Kaleem.Peeroo@city.ac.uk>
2025-04-18 19:44:40 +0100

    Updated config to include ML processing config

--- src/config.py
+++ src/config.py
@@ -4,11 +4,51 @@
 
 LD_DATASETS = [ 
     {
-        "name": "Rel Impact",
-        "exp_folders": \
-                "/Users/kaleem/PhD/Data/5PI/01_Participant Threshold/Participant_Threshold/",
         "ap_config": "",
+        "name": "3PI No Noise",
+        "exp_folders": \
+                "/Users/kaleem/PhD/Datasets/ML/3PI_No_Noise",
         "dataset_path": \
-                "/Users/kaleem/PhD/Datasets/5PI/participant_threshold.parquet",
-    } 
+                "/Users/kaleem/PhD/Datasets/ML/3PI_No_Noise.parquet",
+    },
+    {
+        "ap_config": "",
+        "name": "3PI 6Mbps",
+        "exp_folders": \
+                "/Users/kaleem/PhD/Datasets/ML/3PI_6Mbps",
+        "dataset_path": \
+                "/Users/kaleem/PhD/Datasets/ML/3PI_6Mbps.parquet",
+    },
+    {
+        "ap_config": "",
+        "name": "3PI 8Mbps",
+        "exp_folders": \
+                "/Users/kaleem/PhD/Datasets/ML/3PI_8Mbps",
+        "dataset_path": \
+                "/Users/kaleem/PhD/Datasets/ML/3PI_8Mbps.parquet",
+    },
+    {
+        "ap_config": "",
+        "name": "5PI No Noise",
+        "exp_folders": \
+                "/Users/kaleem/PhD/Datasets/ML/5PI_No_Noise",
+        "dataset_path": \
+                "/Users/kaleem/PhD/Datasets/ML/5PI_No_Noise.parquetj"
+    },
+    {
+        "ap_config": "",
+        "name": "5PI 24Mbps",
+        "exp_folders": \
+                "/Users/kaleem/PhD/Datasets/ML/5PI_24Mbps",
+        "dataset_path": \
+                "/Users/kaleem/PhD/Datasets/ML/5PI_24Mbps.parquet",
+    },
+    {
+        "ap_config": "",
+        "name": "5PI 71Mbps",
+        "exp_folders": \
+                "/Users/kaleem/PhD/Datasets/ML/5PI_71Mbps",
+        "dataset_path": \
+                "/Users/kaleem/PhD/Datasets/ML/5PI_71Mbps.parquet"
+    },
 ]
