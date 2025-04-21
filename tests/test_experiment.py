342d251fe7257e4675823a8fc7211906e6f833ec
Kaleem Peeroo <Kaleem.Peeroo@city.ac.uk>
2025-04-21 19:02:07 +0100

    Added UT for Experiment.pick_best_run()

--- tests/test_experiment.py
+++ tests/test_experiment.py
@@ -11,7 +11,7 @@ from experiment import Experiment
 class TestExperiment:
     def test_process_runs_with_trailing_zeros(self):
         s_test_dir = "./tests/data/test_experiment_with_runs_with_raw"
-        s_exp_name = "600SEC_1B_1PUB_1SUB_BE_MC_100LC"
+        s_exp_name = "600SEC_100B_10PUB_1SUB_REL_MC_0DUR_100LC"
         o_exp = Experiment(
             s_name=s_exp_name,
             ls_csv_paths=[
@@ -31,7 +31,7 @@ class TestExperiment:
             assert os.path.basename(
                 o_exp_run.s_run_name
             ) in ["run1_with_trailing_0", "run2_with_good_data"]
-            assert len(o_exp_run.ls_csv_paths) == 2
+            assert len(o_exp_run.lo_exp_files) == 2
 
     def test_get_run_names(self):
         o_exp = Experiment(
@@ -45,8 +45,6 @@ class TestExperiment:
         )
 
         ls_run_names = o_exp.get_run_names()
-
-        pprint(ls_run_names)
 
         assert len(ls_run_names) == 4
         assert ls_run_names == ["run1", "run2", "run3", "run4"]
@@ -68,6 +66,12 @@ class TestExperiment:
         o_exp.pick_best_run()
 
         assert o_exp.best_exp_run is not None
+        assert len(o_exp.lo_exp_runs) == 2
+        assert o_exp.best_exp_run.s_exp_name == s_exp_name
+        assert os.path.basename(
+            o_exp.best_exp_run.s_run_name
+        ) == "run1_with_trailing_0"
+
 
         assert o_exp.best_run_csv_paths == [
             f"{s_test_dir}/{s_exp_name}/run2_with_good_data/pub_0.csv",
