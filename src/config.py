fd16ab8d1d22a55fb2053487e5b5899538692bc8
Kaleem Peeroo <KaleemPeeroo@gmail.com>
2025-12-02 11:18:28 +0000

    Reformmated config

--- src/config.py
+++ src/config.py
@@ -7,48 +7,48 @@ LD_DATASETS = [
         "ap_config": "",
         "name": "3PI No Noise",
         "exp_folders": \
-                "/Users/kaleem/PhD/Datasets/ML/3PI_No_Noise",
+                "/Users/kaleempeeroo/PhD/Datasets/ML/3PI_No_Noise",
         "dataset_path": \
-                "/Users/kaleem/PhD/Datasets/ML/3PI_No_Noise.parquet",
+                "/Users/kaleempeeroo/PhD/Datasets/ML/3PI_No_Noise.parquet",
     },
     {
         "ap_config": "",
         "name": "3PI 6Mbps",
         "exp_folders": \
-                "/Users/kaleem/PhD/Datasets/ML/3PI_6Mbps",
+                "/Users/kaleempeeroo/PhD/Datasets/ML/3PI_6Mbps",
         "dataset_path": \
-                "/Users/kaleem/PhD/Datasets/ML/3PI_6Mbps.parquet",
+                "/Users/kaleempeeroo/PhD/Datasets/ML/3PI_6Mbps.parquet",
     },
     {
         "ap_config": "",
         "name": "3PI 8Mbps",
         "exp_folders": \
-                "/Users/kaleem/PhD/Datasets/ML/3PI_8Mbps",
+                "/Users/kaleempeeroo/PhD/Datasets/ML/3PI_8Mbps",
         "dataset_path": \
-                "/Users/kaleem/PhD/Datasets/ML/3PI_8Mbps.parquet",
+                "/Users/kaleempeeroo/PhD/Datasets/ML/3PI_8Mbps.parquet",
     },
     {
         "ap_config": "",
         "name": "5PI No Noise",
         "exp_folders": \
-                "/Users/kaleem/PhD/Datasets/ML/5PI_No_Noise",
+                "/Users/kaleempeeroo/PhD/Datasets/ML/5PI_No_Noise",
         "dataset_path": \
-                "/Users/kaleem/PhD/Datasets/ML/5PI_No_Noise.parquet"
+                "/Users/kaleempeeroo/PhD/Datasets/ML/5PI_No_Noise.parquet"
     },
     {
         "ap_config": "",
         "name": "5PI 24Mbps",
         "exp_folders": \
-                "/Users/kaleem/PhD/Datasets/ML/5PI_24Mbps",
+                "/Users/kaleempeeroo/PhD/Datasets/ML/5PI_24Mbps",
         "dataset_path": \
-                "/Users/kaleem/PhD/Datasets/ML/5PI_24Mbps.parquet",
+                "/Users/kaleempeeroo/PhD/Datasets/ML/5PI_24Mbps.parquet",
     },
     {
         "ap_config": "",
         "name": "5PI 71Mbps",
         "exp_folders": \
-                "/Users/kaleem/PhD/Datasets/ML/5PI_71Mbps",
+                "/Users/kaleempeeroo/PhD/Datasets/ML/5PI_71Mbps",
         "dataset_path": \
-                "/Users/kaleem/PhD/Datasets/ML/5PI_71Mbps.parquet"
+                "/Users/kaleempeeroo/PhD/Datasets/ML/5PI_71Mbps.parquet"
     },
 ]
