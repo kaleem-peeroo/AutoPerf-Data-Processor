bf4aea163800dcdc3f04649f145886b6ad4608fd
Kaleem Peeroo <KaleemPeeroo@gmail.com>
2025-12-02 11:08:15 +0000

    Added config for n_runs for UT

---
+++ tests/configs/n_runs.py
@@ -0,0 +1,8 @@
+LD_DATASETS = [
+    {
+        "name": "test campaign with n runs",
+        "exp_folders": "./tests/data/test_campaign_with_n_run_dirs/",
+        "ap_config": "",
+        "dataset_path": "./tests/output/test_campaign_with_n_runs.parquet",
+    }
+]
