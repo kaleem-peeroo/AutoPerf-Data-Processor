53b53a5e1b1ea472d5200e69c4fc65a6b73ff3ed
Kaleem Peeroo <KaleemPeeroo@gmail.com>
2025-12-02 11:09:15 +0000

    Reformatted test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -25,72 +25,96 @@ class TestMain:
 
         with pytest.raises(ValueError):
             validate_config("invalid_type")
-        
+
     def test_validate_config_with_missing_key(self):
         from main import validate_config
 
         with pytest.raises(ValueError):
-            validate_config( [ {"name": "test"} ] )
+            validate_config([{"name": "test"}])
 
     def test_validate_config_with_invalid_key(self):
         from main import validate_config
 
         with pytest.raises(ValueError):
-            validate_config( [ {
-                "name": "test", 
-                "exp_folders": "value",
-                "ap_config": "",
-                "invalid_key": ""
-            } ] )
+            validate_config(
+                [
+                    {
+                        "name": "test",
+                        "exp_folders": "value",
+                        "ap_config": "",
+                        "invalid_key": "",
+                    }
+                ]
+            )
 
     def test_validate_config_with_invalid_value(self):
         from main import validate_config
 
         with pytest.raises(ValueError):
-            validate_config( [ {
-                "name": "test", 
-                "exp_folders": 123,
-                "ap_config": "",
-                "dataset_path": ""
-            } ] )
+            validate_config(
+                [
+                    {
+                        "name": "test",
+                        "exp_folders": 123,
+                        "ap_config": "",
+                        "dataset_path": "",
+                    }
+                ]
+            )
 
     def test_validate_config_with_invalid_dataset_path(self):
         from main import validate_config
 
         with pytest.raises(ValueError):
-            validate_config( [ {
-                "name": "test", 
-                "exp_folders": "",
-                "ap_config": "",
-                "dataset_path": 123
-            } ] )
+            validate_config(
+                [
+                    {
+                        "name": "test",
+                        "exp_folders": "",
+                        "ap_config": "",
+                        "dataset_path": 123,
+                    }
+                ]
+            )
 
     def test_validate_config_with_invalid_ap_config(self):
         from main import validate_config
 
-        assert validate_config( [ {
-            "name": "test", 
-            "exp_folders": "./tests/data/test_campaign_with_csv/",
-            "ap_config": "",
-            "dataset_path": "./tests/output/test_campaign.parquet"
-        } ] ) == [ {
-            "name": "test", 
-            "exp_folders": "./tests/data/test_campaign_with_csv/",
-            "ap_config": "",
-            "dataset_path": "./tests/output/test_campaign.parquet"
-        } ]
+        assert validate_config(
+            [
+                {
+                    "name": "test",
+                    "exp_folders": "./tests/data/test_campaign_with_csv/",
+                    "ap_config": "",
+                    "dataset_path": "./tests/output/test_campaign.parquet",
+                }
+            ]
+        ) == [
+            {
+                "name": "test",
+                "exp_folders": "./tests/data/test_campaign_with_csv/",
+                "ap_config": "",
+                "dataset_path": "./tests/output/test_campaign.parquet",
+            }
+        ]
 
     def test_validate_config_with_existing_dataset_path(self):
         from main import validate_config
 
-        assert validate_config( [ {
-            "name": "test", 
-            "exp_folders": "./tests/data/test_campaign_with_csv",
-            "ap_config": "",
-            "dataset_path": "./tests/output/existing_test_campaign.parquet"
-        } ] ) == [ {
-            "name": "test", 
-            "exp_folders": "./tests/data/test_campaign_with_csv",
-            "ap_config": "",
-            "dataset_path": "./tests/output/existing_test_campaign.parquet"
-        } ]
+        assert validate_config(
+            [
+                {
+                    "name": "test",
+                    "exp_folders": "./tests/data/test_campaign_with_csv",
+                    "ap_config": "",
+                    "dataset_path": "./tests/output/existing_test_campaign.parquet",
+                }
+            ]
+        ) == [
+            {
+                "name": "test",
+                "exp_folders": "./tests/data/test_campaign_with_csv",
+                "ap_config": "",
+                "dataset_path": "./tests/output/existing_test_campaign.parquet",
+            }
+        ]
