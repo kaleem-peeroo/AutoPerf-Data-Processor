11b86670d7bc69a739737ba0b3ef8ed45f5a5938
Kaleem Peeroo <KaleemPeeroo@gmail.com>
2025-12-02 11:08:09 +0000

    Reformatted nromal.py

--- tests/configs/normal.py
+++ tests/configs/normal.py
@@ -1,18 +1,14 @@
-LD_DATASETS = [ 
-   {
+LD_DATASETS = [
+    {
         "name": "test campaign with dirs",
-        "exp_folders": \
-            "./tests/data/test_campaign_with_dirs_simple/",
+        "exp_folders": "./tests/data/test_campaign_with_dirs_simple/",
         "ap_config": "",
-        "dataset_path": \
-            "./tests/output/test_campaign_with_dirs.parquet",
-   },
+        "dataset_path": "./tests/output/test_campaign_with_dirs.parquet",
+    },
     {
         "name": "Rel Impact",
-        "exp_folders": \
-            "./tests/data/test_campaign_with_csv/",
+        "exp_folders": "./tests/data/test_campaign_with_csv/",
         "ap_config": "",
-        "dataset_path": \
-            "./tests/output/test_campaign.parquet",
-    } 
+        "dataset_path": "./tests/output/test_campaign.parquet",
+    },
 ]
